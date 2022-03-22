b9ed52180bf2a4f9ecfdd727b86071da0a6ed0bf
Matheus Rodrigues <85845749+rmathr@users.noreply.github.com>
2022-03-21 21:56:23 -0300

    added collapsed content with buttons

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 function mudaTema() {
   document.body.classList.toggle("dark");
 }
+
+let coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//arrumar click
+
+const buttons = document.querySelectorAll("#btn1, #btn2, #btn3");
+
+for (const btn of buttons) {
+  btn.addEventListener("click", function handleClick() {
+    btn.classList.toggle("clicked");
+  });
+}
+
+// document.addEventListener("click", function handleClick(event) {
+//   event.target.classList.toggle("clicked");
+// });
